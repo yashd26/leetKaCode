d3be8fd216e4bc963848030dfde39fcda2ff7aa3
meep_ <yashd026@gmail.com>
2023-01-03 16:21:35 +0530

    Time: 23 ms (29.77%), Space: 10.1 MB (32.42%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        if(i==0 && j==0){return grid[i][j];}
-        if(i<0 || j<0){return 1e9;}
-        if(dp[i][j]!=-1){return dp[i][j];}
-        return dp[i][j]=min(grid[i][j]+f(i-1, j, grid, dp), grid[i][j]+f(i, j-1, grid, dp));
+    int helper(int inRow, int inCol, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if (inRow < 0 || inCol < 0) {
+            return 1e9;
+        }
+        if (inRow == 0 && inCol == 0) {
+            return grid[inRow][inCol];
+        }
+        if(dp[inRow][inCol] != -1) {
+            return dp[inRow][inCol];
+        }
+        
+        return dp[inRow][inCol] = min(grid[inRow][inCol] + helper(inRow - 1, inCol, grid, dp), grid[inRow][inCol] + helper(inRow, inCol - 1, grid, dp));
     }
+    
     int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size(), m=grid[0].size();
-        vector<vector<int>>dp(n, vector<int>(m, -1));
-        return f(n-1, m-1, grid, dp);
+        int row = grid.size(), col = grid[0].size();
+        vector<vector<int>> dp(row, vector<int>(col, -1));
+        
+        return helper(row - 1, col - 1, grid, dp);
     }
 };
