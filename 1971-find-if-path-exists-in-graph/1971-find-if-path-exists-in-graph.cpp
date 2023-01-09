9e2d138eee82ad1a6ffd1c27d161316db1f1b026
meep_ <yashd026@gmail.com>
2023-01-09 18:59:55 +0530

    Time: 651 ms (84.33%), Space: 221.1 MB (17.27%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, vector<int>& visited, int curr_node, int end_node) {
-        if (curr_node == end_node) {
+    bool helper(vector<vector<int>>& vec, vector<int>& vis, int idx, int val) {
+        if (idx == val) {
             return true;
         }
-        if (visited[curr_node]) {
+        if (vis[idx]) {
             return false;
         }
         
-        visited[curr_node] = 1;
-        
-        for(int i = 0; i < graph[curr_node].size(); i++) {
-            if(dfs(graph, visited, graph[curr_node][i], end_node)) {
+        vis[idx] = 1;
+        for(auto elem: vec[idx]) {
+            if(helper(vec, vis, elem, val)) {
                 return true;
             }
         }
@@ -20,13 +19,13 @@ public:
     }
     
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>> graph(n);
+        vector<vector<int>> vec(n);
         
-        for(int i = 0;i < edges.size(); i++) {
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
+        for(int i = 0; i < edges.size(); i++) {
+            vec[edges[i][0]].push_back(edges[i][1]);
+            vec[edges[i][1]].push_back(edges[i][0]);
         }
-        vector<int> visited(n, 0);
-        return dfs(graph, visited, source, destination);
+        vector<int> vis(n, 0);
+        return helper(vec, vis, source, destination);
     }
 };
